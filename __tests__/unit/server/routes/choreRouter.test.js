db82ff81a1798efd0c09227e1d3bc0026d6d7a99
Brian Barr <brian.barr23@gmail.com>
2021-06-08 15:56:31 -0400

    resolving merge conflict with jest dependency

---
+++ __tests__/unit/server/routes/choreRouter.test.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const choreRouter = require("../../../../server/routes/choreRouter"); //import file we are testing
+const request = require("supertest"); // supertest is a framework that allows to easily test web apis
+const app = express(); //an instance of an express app, a 'fake' express app
+
+app.use("/chores", choreRouter); //routes
+
+describe("testing-chore-routes", () => {
+  it("GET /chores - success", async () => {
+    const { body } = await request(app).get("/chores"); //uses the request function that calls on express app instance
+    expect(body).toEqual([
+      {
+        id: "NJ",
+        createdBy: "Trenton",
+        assignedTo: "Phil Murphy",
+        household: "NJ",
+        description: "Trenton",
+        createdOn: "Phil Murphy",
+        completedOn: "",
+        verifiedOn: "",
+        reward: "",
+      },
+      {
+        id: "NJ",
+        createdBy: "Trenton",
+        assignedTo: "Phil Murphy",
+        household: "NJ",
+        description: "Trenton",
+        createdOn: "Phil Murphy",
+        completedOn: "",
+        verifiedOn: "",
+        reward: "",
+      },
+      {
+        id: "NJ",
+        createdBy: "Trenton",
+        assignedTo: "Phil Murphy",
+        household: "NJ",
+        description: "Trenton",
+        createdOn: "Phil Murphy",
+        completedOn: "",
+        verifiedOn: "",
+        reward: "",
+      },
+
+    ]);
+  });
+});
