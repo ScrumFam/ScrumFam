a792ef6eb43d0c75abbd34bf781069f04330b634
Brian Barr <brian.barr23@gmail.com>
2021-06-09 15:29:38 -0400

    added the user and chore routes, but still missing middleware to make them functional

--- server/routes/choreRouter.js
+++ server/routes/choreRouter.js
@@ -1,12 +1,83 @@
 const { Router } = require('express');
 const router = Router();
+const choreController = require('../controllers/userController')
 
-router.get('/', (req, res) => {
-  res.status(200).json({});
-})
+// /chores
+// middleware:
+//  - DB fucntion to add the chore
+//  - format response object
+//  - Respond w/ the user object 
+router.post('/', 
+  (req, res) => res.json(res.locals.user)
+);
 
-router.post('/', (req, res) => {
-  res.status(200).json({});
-})
+// Get all chores for a household
+// middleware:
+//  - authorize that the user is a parent of the household
+//  - DB fucntion to get all the chores for a specific household
+//  - format response object
+//  - Respond w/ success array of choreObj
+router.get('/house/:householdName',  
+  (req, res) => res.json({})
+);
+
+// Get unassigned chores for a household
+// middleware:
+//  - authorize that the user is a member of the household
+//  - DB function to get all the unassigned chores for a specific household
+//  - format response object
+//  - Respond w/ success array of choreObj
+router.get('/house/:householdName/unassigned',  
+  (req, res) => res.json({})
+);
+
+// Get specific all chores for a specific user
+// middleware:
+//  - authorize is user 
+//  - DB function to get all chores
+//  - format response object
+//  - respond w/ array of choreObj
+router.get('/user/:userId',  
+  (req, res) => res.json({})
+);
+
+// delete a chore
+// middleware:
+//  - authorize that the user is a parent of the household
+//  - DB fucntion to delete the chore
+//  - format response object
+//  - Respond w/ success message  
+router.delete('/:choreId',  
+  (req, res) => res.json({})
+);
+
+// mark Chore Complete
+// middleware: 
+//   - authorize that the user is the one assigned OR parent of household OR member of household if unassigned
+//  - DB call to update chore status w/ completed_at timestamp
+//  - respond w/ sucess message
+router.patch('/:choreId/complete', 
+  (req, res) => res.json({})
+);
+
+// mark Chore incomplete
+// middleware: 
+//  - authorize that the user is the one assigned OR parent of household OR member of household if unassigned
+//  - DB call to update chore status w/ null for completed_at
+//  - respond w/ sucess message
+router.patch('/:choreId/undoComplete', 
+  (req, res) => res.json({})
+);
+
+// assign a chore the users account balance
+// middleware: 
+//  - authorize that the user is the one included or a parent of the household
+//  - DB call to update assigned_to in the row w/ the users userId
+//  - format response object
+//  - respond with choreObject? or success message
+router.put('/:choreId/assign/:userId',
+  (req, res) => res.jsom({})
+);
+
 
 module.exports = router;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,24 +3,65 @@ const router = Router();
 const userController = require('../controllers/userController')
 
 // '/users/
-router.post('/', userController.addUser, async(req, res) => {
-  return res.json(res.locals.userId)
-});
+// middleware:
+//  - house hold authorization?????
+//  - DB fucntion to add the user
+//  - format response object
+//  - Respond w/ the user object 
+router.post('/', 
+  userController.addUser, 
+  (req, res) => res.json(res.locals.user)
+);
 
 // Get all users for a household
-router.get('/house/:householdName',  async (req, res) => {
+// middleware:
+//  - authorize that the user is a parent of the household
+//  - DB fucntion to all the users and their data
+//  - format response object
+//  - Respond w/ success message
+router.get('/house/:householdName',  
+  (req, res) => res.json({})
+);
 
+// Get specific user object
+// middleware:
+//  - authorize is user OR is parent of household
+//  - DB function to get the user object
+//  - format response object
+//  - respond w/ userObj
+router.get('/:userId',  
+  (req, res) => res.json({})
+);
 
-})
+// delete user
+// middleware:
+//  - authorize that the user is a parent of the household
+//  - DB fucntion to delete the user
+//  - format response object
+//  - Respond w/ success or error  
+router.delete('/:userId',  
+  (req, res) => res.json({})
+);
 
+// update user settings
+// middleware: 
+//  - authorize that the user is the one included
+//  - DB call to update the user table
+//  - format response object
+//  - respond w/ updated userObj
+router.patch('/:userId', 
+  (req, res) => res.json({})
+);
 
-router.get('/', (req, res) => {
-  res.status(200).json({});
-});
-
-router.post('/', (req, res) => {
-  res.status(200).jsom({});
-});
+// update the users account balance
+// middleware: 
+//  - authorize that the user is the one included or a parent of the household
+//  - DB call to update balance
+//  - format response object
+//  - respond with new balance 
+router.put('/:userId/balance', 
+  (req, res) => res.jsom({})
+);
 
 
 module.exports = router;
