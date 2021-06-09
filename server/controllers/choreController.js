608dfb909c6ee3f4287ab2adac4ed2cbf8ce6c43
Russell Hayward <russdawg44@gmail.com>
2021-06-09 16:35:15 -0400

    resolving conflicts

--- server/controllers/choreController.js
+++ server/controllers/choreController.js
@@ -1,4 +1,11 @@
-const choreController = {};
-
-
+const db = require("../database/connections");
+const choreController = {
+  getChores(req, res, next) {
+    console.log("inside the getChores controller");
+    async function test() {
+      await db.query(`SELECT * FROM app_user;`);
+    }
+    test();
+  },
+};
 module.exports = choreController;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,4 @@
+const database = require('../database/database');
 
 const userController = {
 
@@ -12,10 +13,23 @@ const userController = {
     return next();
   },
 
-  getUser(req, res, next){
-  
+  async getUser(req, res, next){
+    
   },
 
 };
+
+
+
+
+// '/users' -> all the actions relating to users and households
+
+
+// Get all users for a household (GET `/users/house/:householdName
+// Get a specific user data (GET '/users/:userId')
+// Create a new user (POST '/users'
+// Delete a user (DELETE '/users/:userId'
+// Update a user settings (active_goal, password) (PATCH '/users/:userId')
+// Update balance ("spend" money) (PUT '/users/:userId/balance/`
 
 module.exports = userController;
