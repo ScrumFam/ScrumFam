fda413b607bc41753ceb03b7d1bf779aea482d9d
dbehmoaras@gmail.com <dbehmoaras@gmail.com>
2021-06-10 17:22:43 -0400

    connected front-end to login and signup routes using axios

--- client/Login Page/Login.jsx
+++ client/Login Page/Login.jsx
@@ -1,86 +1,104 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import React from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
 // import { Link as containers } from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import { Link } from "react-router-dom";
+import Paper from '@material-ui/core/Paper'
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
+import axios from 'axios'
 
 // Copyright part at the bottom of the page
 // Word ScrumFam is href that we link to any page
 const linkStyle = {
-    color: 'rgb(37, 150, 190)',
-    fontWeight: 'normal'
+  color: 'rgb(37, 150, 190)',
+  fontWeight: 'normal'
 }
 
-function Copyright() {
+function Copyright () {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography variant='body2' color='textSecondary' align='center'>
       {'Copyright © '}
-      <Link style={linkStyle} to="/">
+      <Link style={linkStyle} to='/'>
         ScrumFam
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
-  );
+  )
 }
 
 // styled component
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    height: '100vh',
+    height: '100vh'
   },
   image: {
-    backgroundImage: 'url(https://r50gh2ss1ic2mww8s3uvjvq1-wpengine.netdna-ssl.com/wp-content/uploads/2019/02/Artboard.svg)',
+    backgroundImage:
+      'url(https://r50gh2ss1ic2mww8s3uvjvq1-wpengine.netdna-ssl.com/wp-content/uploads/2019/02/Artboard.svg)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
     backgroundSize: 'cover',
-    backgroundPosition: 'center',
+    backgroundPosition: 'center'
   },
   paper: {
     margin: theme.spacing(8, 4),
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+    margin: theme.spacing(3, 0, 2)
+  }
+}))
 
-const authObject = {};
+const authObject = {}
 
 const setInputValue = event => {
   console.log(`***EVENT`, event.target.name)
-  authObject[event.target.name] = event.target.value;
+  authObject[event.target.name] = event.target.value
   console.log(`***NEW_AUTH_OBJ:`, authObject)
 }
 
+export default function SignInSide () {
+  const classes = useStyles()
 
-export default function SignInSide() {
-  const classes = useStyles();
+  const history = useHistory()
+
+  const logInUser = () => {
+    axios
+      .post('/users/login', authObject)
+      .then(res => {
+        console.log(`***logInUser:`, res)
+        history.push('/home')
+      })
+      .catch(err => {
+        console.log(`***logInUser:`, err)
+      })
+  }
 
   return (
-    <Grid container component="main" className={classes.root}>
+    <Grid container component='main' className={classes.root}>
       <CssBaseline />
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -88,75 +106,74 @@ export default function SignInSide() {
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
+          <Typography component='h1' variant='h5'>
             Sign in
           </Typography>
           <form className={classes.form} noValidate>
             <TextField
-              variant="outlined"
-              margin="normal"
+              variant='outlined'
+              margin='normal'
               required
               fullWidth
-              id="householdid"
-              label="Household Id"
-              name="householdid"
-              autoComplete="off"
+              id='householdid'
+              label='Household Id'
+              name='household'
+              autoComplete='off'
               autoFocus
-              onChange={(e) => setInputValue(e)}
+              onChange={e => setInputValue(e)}
             />
             <TextField
-              variant="outlined"
-              margin="normal"
+              variant='outlined'
+              margin='normal'
               required
               fullWidth
-              id="email"
-              label="Username"
-              name="email"
-              autoComplete="email"
-              onChange={(e) => setInputValue(e)}
+              id='email'
+              label='Username'
+              name='username'
+              autoComplete='email'
+              onChange={e => setInputValue(e)}
             />
             <TextField
-              variant="outlined"
-              margin="normal"
+              variant='outlined'
+              margin='normal'
               required
               fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={(e) => setInputValue(e)}
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
+              onChange={e => setInputValue(e)}
             />
             <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
+              control={<Checkbox value='remember' color='primary' />}
+              label='Remember me'
             />
             <Button
-              type="submit"
               fullWidth
-              variant="contained"
-              color="primary"
+              variant='contained'
+              color='primary'
               className={classes.submit}
-              onClick={() => {}}
+              onClick={() => logInUser()}
             >
               Sign In
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2" style={linkStyle}>
+                <Link href='#' variant='body2' style={linkStyle}>
                   Forgot password?
                 </Link>
               </Grid>
               <Grid item>
-                <Link to="/signup" variant="body2" style={linkStyle}>
+                <Link to='/signup' variant='body2' style={linkStyle}>
                   {"Don't have an account? Sign Up As Parent"}
                 </Link>
               </Grid>
               <Grid item>
                 {/* this home link can be deleted. it is only to test the homepage */}
                 <br />
-                <Link to="/home" variant="body2" style={linkStyle}>
-                  {"HOME_TEST"}
+                <Link to='/home' variant='body2' style={linkStyle}>
+                  {'HOME_TEST'}
                 </Link>
               </Grid>
             </Grid>
@@ -167,5 +184,5 @@ export default function SignInSide() {
         </div>
       </Grid>
     </Grid>
-  );
+  )
 }
